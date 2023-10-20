962d0d8527c723bcec971995198d78c41c3c4463
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2023-10-20 12:07:57 +0100

    Add SoftmaxLikelihood

--- src/util.py
+++ src/util.py
@@ -7,6 +7,50 @@ import lab as B
 
 
 
+
+
+
+class SoftmaxLikelihood(nps.likelihood.AbstractLikelihood):
+    """
+    SoftmaxLikelihood class represents a softmax likelihood function.
+    Essentially a list of probabilities that sum up to 1.
+
+    Parameters
+    ----------
+    k : int
+        Number of available actions.
+    """
+
+    def __init__(self, k=4):
+        """
+        Initializes the SoftmaxLikelihood class with the provided number of
+        available actions.
+
+        Parameters
+        ----------
+        k : int (Default: 4)
+            Number of available actions.
+        """
+        self.k = k
+
+    def __call__(self, x):
+        """
+        Computes the action probabilities based on the input tensor by running
+        a softmax.
+
+        Parameters
+        ----------
+        x : tensor
+            Input tensor.
+
+        Returns
+        -------
+        Categorical
+            Categorical distribution generated from the computed probabilities.
+        """
+        p = B.softmax(x, axis=1)
+        return Categorical(probs=p)
+
     
     
 
