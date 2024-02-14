4bd96299bf8367afe826078d9dc21dbf1f60d139
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2024-02-14 13:51:46 +0000

    Remove Categorical class

--- dppum/util.py
+++ dppum/util.py
@@ -14,26 +14,6 @@ import lab as B
 
 
 
-class Categorical:
-    """
-    A class representing a categorical distribution.
-
-    Parameters
-    ----------
-    p : array-like
-        Event probabilities.
-    """
-
-    def __init__(self, p):
-        """
-        Initializes the Categorical distribution with the provided event probabilities.
-
-        Parameters
-        ----------
-        p : array-like
-            Event probabilities.
-        """
-        self.p = p
 
 
 
