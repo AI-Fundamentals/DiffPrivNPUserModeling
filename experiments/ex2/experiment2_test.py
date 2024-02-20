3d0a70e09a36ca5e22ae9644d3e52914d0ceb01f
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2024-02-20 15:08:04 +0000

    Add command line arguments

--- experiments/ex2/experiment2_test.py
+++ experiments/ex2/experiment2_test.py
@@ -16,3 +16,42 @@ from dppum.util import print_dictionary
 from dppum.train import train_model_dp_tf
 
 print("Finished importing packages.")
+
+# %%
+# Parse any command line arguments
+
+# Creating the ArgumentParser instance
+parser = argparse.ArgumentParser()
+
+# Adding arguments to the parser
+parser.add_argument("--test_hdf", 
+                    help="The file to load the test from.", 
+                    type=str,
+                    default="data/ex2/experiment2_test_data.hdf")
+
+parser.add_argument("--models_dir", 
+                    help="The folder to load the trained models.", 
+                    type=str,
+                    default="models/ex2/")
+
+parser.add_argument("--figs_dir", 
+                    help="The folder for output figures.", 
+                    type=str,
+                    default="figures/ex2/")
+
+
+# Parsing the arguments to a dictionary
+args = vars(parser.parse_args())
+
+# Save the command line args to a json in model save folder
+if args['models_dir']:
+    # Check if the directory exists
+    if not os.path.exists(args['models_dir']):
+        # If not, create the directory
+        os.makedirs(args['models_dir'])
+    
+    # Write command line arguments to JSON file
+    with open(os.path.join(args['models_dir'], "test_command_line_args.json"), 'w') as json_file:
+        json.dump(args, json_file,indent=4)
+
+print("Finished parsing command line arguments.")
