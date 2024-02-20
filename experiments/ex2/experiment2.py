35da8f562da43e0e38f207bfa030535200b21f68
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2024-02-20 12:15:51 +0000

    Update experiment2 training script

--- experiments/ex2/experiment2.py
+++ experiments/ex2/experiment2.py
@@ -1,25 +1,22 @@
 #Load local copy of neuralprocesses if present
-import sys
-sys.path.insert(0,'/Users/user/github/neuralprocesses')
+#import sys
+#sys.path.insert(0,'/Users/user/github/neuralprocesses')
 
 import neuralprocesses.tensorflow as nps
 import argparse
 import os
 import tensorflow as tf
 import tensorflow.keras.backend as K
-from tensorflow_privacy import DPKerasAdamOptimizer
 import pdb
 import lab as B
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 
 
 from dppum.data import hdf_to_tf_dataset
-from dppum.loss import np_elbo_tf_cat, np_elbo_explicit
-from dppum.util import calc_cat_confidence, flatten_first_two_dims, print_dictionary
-from dppum.privacy_oracle import get_sigma_from_privacy_loss_distribution as get_sigma
-from dppum.train import train_model_dp
+from dppum.loss import np_elbo_tf_cat
+from dppum.util import print_dictionary, calc_cat_acc_onehot
+from dppum.train import train_model_dp_tf
 
 # %%
 
@@ -33,22 +30,27 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--num_batches", 
                     help="Number of batches to use from the training data hdf.", 
                     type=int, 
-                    default=192)
+                    default=4)
 
 parser.add_argument("--hdf", 
                     help="Directly specify the file to load the training from.", 
                     type=str,
                     default="data/ex2/experiment2_training_data.hdf")
 
-parser.add_argument("--models", 
+parser.add_argument("--models_dir", 
                     help="Directly specify the folder to load the trained models from.", 
                     type=str,
-                    default="data/ex2/experiment2_training_data.hdf")
+                    default="models/ex2/")
 
 parser.add_argument("--fig", 
                     help="Directly specify the folder for output figures.", 
                     type=str,
                     default="figures/ex2/")
+
+parser.add_argument("--warmup_epoch", 
+                    help="Use a warmup epoch (True/False)", 
+                    type=bool,
+                    default=False)
 
 # Parsing the arguments
 args = parser.parse_args()
@@ -62,11 +64,11 @@ print(f"\nOriginal metadata for file '{args.hdf}':")
 print_dictionary(dataset_metadata)
 
 
-num_batches = 4  # replace with your desired number of batches
-dataset = dataset.shuffle(dataset_metadata['n_minibatches']).take(num_batches)
-dataset_metadata['n_minibatches'] = num_batches
+dataset = dataset.shuffle(dataset_metadata['n_minibatches']).take(args.num_batches)
+dataset_metadata['n_minibatches'] = args.num_batches
 num_users = len(list(dataset)) * dataset_metadata['batch_size']
-print(f"Dataset prepared for {num_users} users, in {num_batches} (mini)batches of size {dataset_metadata['batch_size']}.")
+dataset_metadata['n_users'] = num_users
+print(f"Dataset prepared for {num_users} users, in {args.num_batches} (mini)batches of size {dataset_metadata['batch_size']}.")
 print(f"\nUpdated metadata for file '{args.hdf}':")
 print_dictionary(dataset_metadata)
 
@@ -87,17 +89,12 @@ K.clear_session()
 # so I think that means het rather than lowrank
 # dim_lv is about a latent variable for non-Gaussianity (LNP), so set to 0
 
-model = nps.construct_agnp(dim_x=17, dim_y=9, dim_lv=0, dim_embedding=16,
+model_ex2 = nps.construct_agnp(dim_x=17, dim_y=9, dim_lv=0, dim_embedding=16,
                         num_enc_layers=6,num_dec_layers=6,
                         likelihood="het",
-                        nonlinearity='LeakyReLU') #This improves training if true
+                        nonlinearity='LeakyReLU')
 
 print("Running with dim_embedding = 16 for speed but it should be 128")
-
-optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=5e-4)
-
-
-
 
 # Number of samples taken to assess sample accuracy/confidence
 num_samples=5
@@ -105,17 +102,12 @@ num_samples=5
 # Flag for a warmup epoch (i.e. testing the untrained model)
 # This will be epoch 0
 warmup_epoch = False
-if warmup_epoch:
-    first_epoch = 0
-else:
-    first_epoch = 1
-
 
 # %% 
 # Train model using train_model_dp function
 
-history = train_model_dp(
-    model,
+history = train_model_dp_tf(
+    model_ex2,
     dataset,
     dataset_metadata,
     loss_fn=np_elbo_tf_cat,
@@ -124,11 +116,12 @@ history = train_model_dp(
     clipping_bound=2,
     optimizer_name='Adam',
     learning_rate=5e-4,
-    dp_enc=True,
+    dp_enc=False,
     dp_dec=False,
     num_samples=5,
     warmup_epoch=False,
-    shuffle=True,    
+    shuffle=True,
+    model_save_dir = args.models_dir
     )
 
 
@@ -173,3 +166,38 @@ if not os.path.exists(figures_path):
 
 # Save the figure
 fig.savefig(figures_path+'experiment2_training_metrics.png')
+
+
+# %%
+#testing loading a model from saved weights
+
+iterator = iter(dataset)
+xc,yc,xt,yt = next(iterator)
+yc_t = B.transpose(yc,perm=[0,2,1])
+yt_t = B.transpose(yt,perm=[0,2,1])
+
+
+
+model2 = nps.construct_agnp(dim_x=17, dim_y=9, dim_lv=0, dim_embedding=16,
+                        num_enc_layers=6,num_dec_layers=6,
+                        likelihood="het",
+                        nonlinearity='LeakyReLU')
+
+mean, _, _, _ = nps.predict(
+    model2,xc, yc_t, xt, num_samples=1
+    )
+
+acc = calc_cat_acc_onehot(yt_t,mean,cat_axis=-2)
+print(f"Accuracy of untrained model: {acc}")
+
+
+model2.load_weights('models/ex2/weights_epoch_5.tf')
+
+mean, _, _, _ = nps.predict(
+    model2,xc, yc_t, xt, num_samples=1
+    )
+
+acc = calc_cat_acc_onehot(yt_t,mean,cat_axis=-2)
+print(f"Accuracy of loaded model: {acc}")
+
+
