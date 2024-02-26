f22db8b93dcac34d4f632c0c853060245be055b7
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2024-02-26 14:02:50 +0000

    Update test_nps_tf to be an actual test

--- tests/test_nps_tf.py
+++ tests/test_nps_tf.py
@@ -3,23 +3,24 @@
 # Example from https://wessel.ai/neuralprocesses/basic_usage.html
 # Tensorflow version
 
-try:
-    import lab as B
-    import tensorflow as tf
-    import neuralprocesses.tensorflow as nps    
-
-    cnp = nps.construct_gnp(dim_x=2, dim_y=3, likelihood="lowrank")
-    dist = cnp(
-        B.randn(tf.float32, 16, 2, 10),  # Context inputs
-        B.randn(tf.float32, 16, 3, 10),  # Context outputs
-        B.randn(tf.float32, 16, 2, 15),  # Target inputs
-    )
-    mean, var = dist.mean, dist.var  # Prediction for target outputs
-
-    print(dist.logpdf(B.randn(tf.float32, 16, 3, 15)))
-    print(dist.sample())
-    print(dist.kl(dist))
-    print(dist.entropy())
-
-except Exception as e:
-    print(f"An error occurred: {e}")
+def test_nps_tf():
+    try:
+        import lab as B
+        import tensorflow as tf
+        import neuralprocesses.tensorflow as nps    
+    
+        cnp = nps.construct_gnp(dim_x=2, dim_y=3, likelihood="lowrank")
+        dist = cnp(
+            B.randn(tf.float32, 16, 2, 10),  # Context inputs
+            B.randn(tf.float32, 16, 3, 10),  # Context outputs
+            B.randn(tf.float32, 16, 2, 15),  # Target inputs
+        )
+        mean, var = dist.mean, dist.var  # Prediction for target outputs
+    
+        print(dist.logpdf(B.randn(tf.float32, 16, 3, 15)))
+        print(dist.sample())
+        print(dist.kl(dist))
+        print(dist.entropy())
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
