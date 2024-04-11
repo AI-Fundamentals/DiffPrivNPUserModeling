863d42e97ccc6fbdf330ea2ae51cabdedc217410
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2024-04-11 14:09:56 +0100

    Add test_AverageMeter

--- tests/test_train_torch.py
+++ tests/test_train_torch.py
@@ -4,6 +4,12 @@ def test_get_device_type():
     device = get_device_type()
     assert isinstance(device, str), "Device must be a string"
     
-
+def test_AverageMeter():
+    averagemeter = AverageMeter()
+    
+    averagemeter.update(2)
+    averagemeter.update(4)
+    
+    assert averagemeter.result() == 3
     
     
