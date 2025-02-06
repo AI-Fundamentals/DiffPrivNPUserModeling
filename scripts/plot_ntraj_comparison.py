0692c89ae4e4cd94c08d4cba60ee78adbc7d6e3d
Jonathan Taylor <jonathan.taylor@manchester.ac.uk>
2025-02-06 14:41:30 +0000

    Script to plot eval_ntraj

--- scripts/plot_ntraj_comparison.py
+++ scripts/plot_ntraj_comparison.py
@@ -1,7 +1,6 @@
 """
-Script to go through various folders of model runs,
-work out the best epoch (i.e. highest accuracy),
-and plot for the different values of epsilon.
+Script to go through various folders of model runs
+and compare different ntraj evaluations.
 """
 
 import pandas as pd
@@ -10,74 +9,36 @@ import matplotlib.pyplot as plt
 # %%
 eps_list = ["nodp","eps1","eps3","eps5","eps10"]
 labels = ["non-DP","eps1","eps3","eps5","eps10"]
-num_train_users_list = [50,100,150,300,600,900,1200,1500,1800]
+colors = ['k', 'firebrick', 'orange', 'limegreen', 'blue']
 
 # %%
 
-df_best_epoch = pd.DataFrame(index=num_train_users_list,columns=eps_list)
-df_best_acc = pd.DataFrame(index=num_train_users_list,columns=eps_list)
-df_best_acc_Q25 = pd.DataFrame(index=num_train_users_list,columns=eps_list)
-df_best_acc_Q75 = pd.DataFrame(index=num_train_users_list,columns=eps_list)
+fig,ax = plt.subplots()
 
-for eps in eps_list:
-    for num_train_users in num_train_users_list:
-        try:
-            # Load CSV
-            filepath_train = f'models/ex2/{num_train_users}/{eps}/training_metrics.csv'
-            filepath_val = f'models/ex2/{num_train_users}/{eps}/eval_acc_vs_epochs.csv'
-            df_train = pd.read_csv(filepath_train)
-            df_train.set_index('epoch',inplace=True)
-            df_val = pd.read_csv(filepath_val)
-            df_val.set_index('epoch',inplace=True)
+num_train_users = 50
+for eps,color,label in zip(eps_list,colors,labels):
+        # Load CSV
+        filepath_eval_ntraj = f'models/ex2/{num_train_users}/{eps}/eval_acc_vs_ntraj.csv'
         
-            # Find the best epoch
-            #best_epoch = df_val['acc_sample_mean'].argmax()
-            best_epoch = df_train['loss'].argmin()
-            best_acc = df_val['acc_sample_mean'][best_epoch]
-            best_acc_Q25 = df_val['acc_sample_Q25'][best_epoch]
-            best_acc_Q75 = df_val['acc_sample_Q75'][best_epoch]
-            
-            df_best_epoch.loc[num_train_users][eps] = best_epoch
-            df_best_acc.loc[num_train_users][eps] = best_acc
-            df_best_acc_Q25.loc[num_train_users][eps] = best_acc_Q25
-            df_best_acc_Q75.loc[num_train_users][eps] = best_acc_Q75
-            
-        except:
-            pass
+        df_eval_ntraj = pd.read_csv(filepath_eval_ntraj)
+        df_eval_ntraj.set_index('n_traj',inplace=True)
+        
+        ax.plot(df_eval_ntraj.index,df_eval_ntraj['acc_sample_mean'], marker='o', color=color, linestyle='--', label=label)
+        ax.fill_between(df_eval_ntraj.index, 
+                        df_eval_ntraj['acc_sample_Q25'], 
+                        df_eval_ntraj['acc_sample_Q75'], 
+                        color=color, 
+                        alpha=0.1)
 
-
-df_best_epoch.to_csv('models/ex2/ex2_best_epochs.csv')
-df_best_acc.to_csv('models/ex2/ex2_best_acc_mean.csv')
-df_best_acc_Q25.to_csv('models/ex2/ex2_best_acc_Q25.csv')
-df_best_acc_Q75.to_csv('models/ex2/ex2_best_acc_Q75.csv')
-
-
-# %% Convert the dataframes to numeric type to avoid type issues
-df_best_acc = df_best_acc.apply(pd.to_numeric)
-df_best_acc_Q25 = df_best_acc_Q25.apply(pd.to_numeric)
-df_best_acc_Q75 = df_best_acc_Q75.apply(pd.to_numeric)
-
-# %% Make the plot
-colors = ['k', 'firebrick', 'orange', 'limegreen', 'blue']
-
-# Plot the mean
-ax = df_best_acc.plot(kind='line', marker='o', color=colors, linestyle='--')
-
-# Plot the error bars
-for eps, color in zip(eps_list,colors):
-    ax.fill_between(num_train_users_list, 
-                    df_best_acc_Q25[eps], 
-                    df_best_acc_Q75[eps], 
-                    color=color, 
-                    alpha=0.1)
+ax.legend(fontsize=16)
 
 # Set the labels and title, legend etc.
-plt.legend(labels,fontsize=16)
-plt.xlabel('Users seen',fontsize=18)
+plt.xlabel('Behaviours seen',fontsize=18)
 plt.ylabel('Accuracy',fontsize=18)
 plt.grid(True)
 plt.tight_layout()  
 
 # Show the plot
-plt.savefig('figures/ex2/ex2_val_acc_best_epoch.png',dpi=200)
+plt.savefig('figures/ex2/ex2_eval_ntraj.png',dpi=200)
 plt.show()
+
